6bdac358e46bcb7849a5cb703c6d16974e4abf43
shen yongjian <shenyongjian@jinjianghotels.com.cn>
2024-02-19 15:20:55 +0800

    Site updated: 2024-02-19 15:20:54

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/02/19/hello-world/","2024/02/19/从0到1搭建自己的hexo博客/","2024/02/19/第一篇文章/"];function toRandomPost(){
+var posts=["2024/02/19/hello-world/","2024/02/19/从0到1搭建自己的hexo博客/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
