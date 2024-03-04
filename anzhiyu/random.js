82d13c4c06294520e1f9f7faa833682573e58050
shen yongjian <shenyongjian@jinjianghotels.com.cn>
2024-03-04 17:19:20 +0800

    Site updated: 2024-03-04 17:19:18

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/02/19/从0到1搭建自己的hexo博客/"];function toRandomPost(){
+var posts=["2024/02/19/从0到1搭建自己的hexo博客/","2024/02/21/网络编程懒人入门-二-：快速理解网络通信协议（下篇）/","2024/02/21/网络编程懒人入门-一-：快速理解网络通信协议（上篇）/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
